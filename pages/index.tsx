705af8b258e09aace2058168b200aab66d8cd155
Carter Black <carterb@mailbox.org>
2021-07-14 01:57:59 -0700

    spotify

--- pages/index.tsx
+++ pages/index.tsx
@@ -318,25 +318,17 @@ export default function Home() {
 					'max-w-4xl mx-auto py-16 px-4 sm:px-6 lg:px-8 space-y-8 font-mono'
 				)}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
+				<a
+					href="https://sptfy.com/carter"
 					className={classNames(
 						color.hoverText,
 						color.accent,
 						'h-6 w-6'
 					)}
-					onClick={() => rePickElements()}
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
 				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						strokeWidth={2}
-						d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-					/>
-				</svg>
+					Trade playlists with me on Spotify
+				</a>
+				<p>(also try clicking my name above)</p>
 			</div>
 		</Layout>
 	);
