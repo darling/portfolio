bdcf86959ceae9edf73d69abc6e22bcf7f468c01
Carter B <lovable@riseup.net>
2021-10-23 02:03:02 -0700

    project meta done

--- data/projects.ts
+++ data/projects.ts
@@ -12,21 +12,21 @@ export const global: IPortfolioProjects = {
 			title: 'Shoku',
 			description: 'Another Discord bot engagement platform',
 			link: 'https://shoku.gg/',
-			image: 'https://i.imgur.com/FcwIX1a.png',
+			image: 'https://i.imgur.com/YSEZ0I7.png',
 		},
 		{
 			title: 'Bruhhouse',
 			description:
 				'A site for artists to generate character profiles in html',
 			link: 'https://bruhhouse.vercel.app/',
-			image: 'https://i.imgur.com/FcwIX1a.png',
+			image: 'https://i.imgur.com/P1Xc4VB.png',
 		},
 		{
 			title: 'Unworthy',
 			description:
 				'A group of people dedicated to providing sustainable free-speech resources',
 			link: 'https://unworthy.net/',
-			image: 'https://i.imgur.com/FcwIX1a.png',
+			image: 'https://i.imgur.com/SXidSlB.png',
 		},
 	],
 };
